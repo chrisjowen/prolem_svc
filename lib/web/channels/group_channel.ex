171c909c06f9cd093ed4343922d96eaaef6ef387
Chris Owen <chris.j.owen@hotmail.co.uk>
2022-10-04 00:20:50 +0800

    Sffo

--- lib/web/channels/group_channel.ex
+++ lib/web/channels/group_channel.ex
@@ -4,16 +4,17 @@ defmodule Totem.GroupChannel do
 
   def join("group:" <> group_id, _params, socket) do
     {group_id, _} = Integer.parse(group_id)
-    {:ok, assign(socket, :current_event, group_id)}
+    {:ok, assign(socket, :current_group, group_id)}
   end
 
   def handle_in("msg", msg, socket) do
+    IO.inspect("GOT MESSAES")
     with {:ok, message} <-
            GroupChatRepo.insert(%{
-             group_id: socket.assigns[:current_event],
+             group_id: socket.assigns[:current_group],
              user_id: socket.assigns[:current_user],
              media_id: Map.get(msg, "media_id"),
-             msg: Map.get(msg, "body")
+             msg: Map.get(msg, "msg")
            }),
          message <- GroupChatRepo.get(message.id, [:user, :media]) do
       broadcast!(socket, "msg", message)
