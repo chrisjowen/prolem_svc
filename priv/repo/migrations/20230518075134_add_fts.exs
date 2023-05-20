24c2921f125b2beb42b11a21e32522d6705ad8c2
Chris Owen <chris.j.owen@hotmail.co.uk>
2023-05-20 21:10:51 +0200

    Updated migrations

--- priv/repo/migrations/20230518075134_add_fts.exs
+++ priv/repo/migrations/20230518075134_add_fts.exs
@@ -4,11 +4,9 @@ defmodule ProblemService.Repo.Migrations.AddFts do
 
   def up do
     execute("CREATE EXTENSION IF NOT EXISTS pg_trgm")
-    execute "CREATE INDEX problems_overview_trgm_index ON problems USING gin (overview gin_trgm_ops);"
   end
 
   def down do
     execute("DROP EXTENSION IF EXISTS pg_trgm")
-    execute "DROP INDEX users_username_trgm_index;"
   end
 end
