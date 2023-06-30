d5bb334bc48113872c95f506457a7e99f2711c0e
Chris Owen <chris.j.owen@hotmail.co.uk>
2023-06-30 16:10:14 +0800

    issue with migration

--- priv/repo/migrations/20230629151517_update_idea_add_estimated_timescale.exs
+++ priv/repo/migrations/20230629151517_update_idea_add_estimated_timescale.exs
@@ -2,12 +2,8 @@ defmodule ProblemService.Repo.Migrations.UpdateIdeaAddEstimatedTimescale do
   use Ecto.Migration
 
   def change do
-    create table(:idea_sectors) do
-      add :idea_id, references(:ideas, on_delete: :nothing)
-      add :sector_id, references(:sectors, on_delete: :nothing)
+    alter table(:ideas) do
+      add :estimated_timescale, :integer
     end
-
-    create index(:idea_sectors, [:idea_id])
-    create index(:idea_sectors, [:sector_id])
   end
 end
